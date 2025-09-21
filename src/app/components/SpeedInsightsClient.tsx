6cabf6e995b4189eec5b122f870811247ac59647
DavsonMelo <davsonmelo@protonmail.com>
2025-09-20 21:11:23 -0300

    Configurando SpeedInsights no client component

---
+++ src/app/components/SpeedInsightsClient.tsx
@@ -0,0 +1,8 @@
+// blogfolio/src/components/SpeedInsightsClient.tsx
+"use client";
+
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+export function SpeedInsightsClient() {
+  return <SpeedInsights />;
+}
